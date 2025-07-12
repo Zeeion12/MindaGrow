34dd4c8978c4e12b5651dc7d99875496cdb361fb
Dimasrizky <dimasrizky822@gmail.com>
2025-07-12 11:41:00 +0700

    Update GameMainUI.jsx

--- src/pages/games/GameMainUI.jsx
+++ src/pages/games/GameMainUI.jsx
@@ -207,7 +207,7 @@ export default function GameMainUI() {
 
                 {/* Content */}
                 <div>
-                    <h2 className='text-xl font-poppins font-semibold mb-4'>Lanjutkan Progress Game-mu!</h2>
+                    <h2 className='text-xl font-poppins font-semibold mb-4'>Daftar Game</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {gameData.map((game) => (
                             <Game
